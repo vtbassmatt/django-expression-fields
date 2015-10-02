655343aa5e2504ffe4940539d753ffe08727fd5f
Matt Cooper <vtbassmatt@gmail.com>
2015-10-01 21:32:33 -0700

    added additional testing

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -32,3 +32,116 @@ class DivideDecimalTests(unittest.TestCase):
         self.assertFalse(form4.is_valid())
         self.assertTrue(form5.is_valid())
         self.assertFalse(form6.is_valid())
+
+    def test_divide(self):
+        """
+        Test a few instances of fields with a '/'.
+        """
+        form1 = TesterForm({'field1': '1/2', 'field2': '2/1', 'field3': '3/3'})
+        self.assertTrue(form1.is_valid())
+        self.assertEqual(form1.cleaned_data['field1'], Decimal('.5'))
+        self.assertEqual(form1.cleaned_data['field2'], Decimal('2'))
+        self.assertEqual(form1.cleaned_data['field3'], Decimal('1'))
+
+        form2 = TesterForm({'field1': '1/3', 'field2': '1/3', 'field3': '1/3'})
+        self.assertTrue(form2.is_valid())
+        self.assertEqual(form2.cleaned_data['field1'], Decimal('.33'))
+        self.assertEqual(form2.cleaned_data['field2'], Decimal('.3'))
+        self.assertEqual(form2.cleaned_data['field3'], Decimal('0'))
+
+        form3 = TesterForm({'field1': '1/4', 'field2': '1/4', 'field3': '1/4'})
+        self.assertTrue(form3.is_valid())
+        self.assertEqual(form3.cleaned_data['field1'], Decimal('.25'))
+        self.assertEqual(form3.cleaned_data['field2'], Decimal('.2'))
+        self.assertEqual(form3.cleaned_data['field3'], Decimal('0'))
+
+        form4 = TesterForm({'field1': '1/2', 'field2': '1/2', 'field3': '1/2'})
+        self.assertTrue(form4.is_valid())
+        self.assertEqual(form4.cleaned_data['field1'], Decimal('.5'))
+        self.assertEqual(form4.cleaned_data['field2'], Decimal('.5'))
+        self.assertEqual(form4.cleaned_data['field3'], Decimal('0'))
+
+    def test_negative_numerators(self):
+        """
+        Test a few instances of negative numerators.
+        """
+        formN1 = TesterForm({'field1': '-1/2', 'field2': '-2/1', 'field3': '-3/3'})
+        self.assertTrue(formN1.is_valid())
+        self.assertEqual(formN1.cleaned_data['field1'], Decimal('-.5'))
+        self.assertEqual(formN1.cleaned_data['field2'], Decimal('-2'))
+        self.assertEqual(formN1.cleaned_data['field3'], Decimal('-1'))
+
+        formN2 = TesterForm({'field1': '-1/3', 'field2': '-1/3', 'field3': '-1/3'})
+        self.assertTrue(formN2.is_valid())
+        self.assertEqual(formN2.cleaned_data['field1'], Decimal('-.33'))
+        self.assertEqual(formN2.cleaned_data['field2'], Decimal('-.3'))
+        self.assertEqual(formN2.cleaned_data['field3'], Decimal('-0'))
+
+        formN3 = TesterForm({'field1': '-1/4', 'field2': '-1/4', 'field3': '-1/4'})
+        self.assertTrue(formN3.is_valid())
+        self.assertEqual(formN3.cleaned_data['field1'], Decimal('-.25'))
+        self.assertEqual(formN3.cleaned_data['field2'], Decimal('-.2'))
+        self.assertEqual(formN3.cleaned_data['field3'], Decimal('0'))
+
+        formN4 = TesterForm({'field1': '-1/2', 'field2': '-1/2', 'field3': '-1/2'})
+        self.assertTrue(formN4.is_valid())
+        self.assertEqual(formN4.cleaned_data['field1'], Decimal('-.5'))
+        self.assertEqual(formN4.cleaned_data['field2'], Decimal('-.5'))
+        self.assertEqual(formN4.cleaned_data['field3'], Decimal('0'))
+
+    def test_negative_denominators(self):
+        """
+        Test a few instances of negative numerators.
+        """
+        formN1 = TesterForm({'field1': '1/-2', 'field2': '2/-1', 'field3': '3/-3'})
+        self.assertTrue(formN1.is_valid())
+        self.assertEqual(formN1.cleaned_data['field1'], Decimal('-.5'))
+        self.assertEqual(formN1.cleaned_data['field2'], Decimal('-2'))
+        self.assertEqual(formN1.cleaned_data['field3'], Decimal('-1'))
+
+        formN2 = TesterForm({'field1': '1/-3', 'field2': '1/-3', 'field3': '1/-3'})
+        self.assertTrue(formN2.is_valid())
+        self.assertEqual(formN2.cleaned_data['field1'], Decimal('-.33'))
+        self.assertEqual(formN2.cleaned_data['field2'], Decimal('-.3'))
+        self.assertEqual(formN2.cleaned_data['field3'], Decimal('-0'))
+
+        formN3 = TesterForm({'field1': '1/-4', 'field2': '1/-4', 'field3': '1/-4'})
+        self.assertTrue(formN3.is_valid())
+        self.assertEqual(formN3.cleaned_data['field1'], Decimal('-.25'))
+        self.assertEqual(formN3.cleaned_data['field2'], Decimal('-.2'))
+        self.assertEqual(formN3.cleaned_data['field3'], Decimal('0'))
+
+        formN4 = TesterForm({'field1': '1/-2', 'field2': '1/-2', 'field3': '1/-2'})
+        self.assertTrue(formN4.is_valid())
+        self.assertEqual(formN4.cleaned_data['field1'], Decimal('-.5'))
+        self.assertEqual(formN4.cleaned_data['field2'], Decimal('-.5'))
+        self.assertEqual(formN4.cleaned_data['field3'], Decimal('0'))
+
+    def test_divide_negative_by_negative(self):
+        """
+        Ensure that -/- is positive.
+        """
+        form1 = TesterForm({'field1': '-1/-2', 'field2': '-2/-1', 'field3': '-3/-3'})
+        self.assertTrue(form1.is_valid())
+        self.assertEqual(form1.cleaned_data['field1'], Decimal('.5'))
+        self.assertEqual(form1.cleaned_data['field2'], Decimal('2'))
+        self.assertEqual(form1.cleaned_data['field3'], Decimal('1'))
+
+        form2 = TesterForm({'field1': '-1/-3', 'field2': '-1/-3', 'field3': '-1/-3'})
+        self.assertTrue(form2.is_valid())
+        self.assertEqual(form2.cleaned_data['field1'], Decimal('.33'))
+        self.assertEqual(form2.cleaned_data['field2'], Decimal('.3'))
+        self.assertEqual(form2.cleaned_data['field3'], Decimal('0'))
+
+        form3 = TesterForm({'field1': '-1/-4', 'field2': '-1/-4', 'field3': '-1/-4'})
+        self.assertTrue(form3.is_valid())
+        self.assertEqual(form3.cleaned_data['field1'], Decimal('.25'))
+        self.assertEqual(form3.cleaned_data['field2'], Decimal('.2'))
+        self.assertEqual(form3.cleaned_data['field3'], Decimal('0'))
+
+        form4 = TesterForm({'field1': '-1/-2', 'field2': '-1/-2', 'field3': '-1/-2'})
+        self.assertTrue(form4.is_valid())
+        self.assertEqual(form4.cleaned_data['field1'], Decimal('.5'))
+        self.assertEqual(form4.cleaned_data['field2'], Decimal('.5'))
+        self.assertEqual(form4.cleaned_data['field3'], Decimal('0'))
+
